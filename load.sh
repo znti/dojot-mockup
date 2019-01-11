52c22960177bcf4ec27bc53d6bc380ef8b5f00c2
Zanetti <znti.rafael@gmail.com>
2019-01-11 17:00:28 -0200

    Add deviceType property to allow differentiation

--- load.sh
+++ load.sh
@@ -2,15 +2,15 @@
 
 serverUrl="localhost:8080"
 
-totalDevices=1000
-messagesPerDevice=1000
+totalDevices=1
+messagesPerDevice=1
 
 for i in $(seq ${totalDevices}); do
 	deviceId="device_$(printf "%04d" ${i})"
 
 	echo "Creating device ${deviceId}"
 
-	curl -X POST -H 'Content-Type:application/json' -d "{\"deviceId\":\"${deviceId}\"}" "${serverUrl}/devices"
+	curl -X POST -H 'Content-Type:application/json' -d "{\"deviceType\": \"mock\", \"deviceId\":\"${deviceId}\"}" "${serverUrl}/devices"
 
 	echo ""
 done;
